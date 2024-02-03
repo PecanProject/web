de2490b2fd1395f9c1bba1b7a27f4d81ba586361
Mustafa Azad <97380192+mustafaazad03@users.noreply.github.com>
2024-02-03 10:55:10 +0530

    White space removed develop.js

--- src/pages/documentation/develop.js
+++ src/pages/documentation/develop.js
@@ -3,29 +3,30 @@ import Layout from "@theme/Layout";
 import "./documentation.css";
 import Foot from "@docusaurus/Head";
 function Develop() {
-	return (
-		<>
-			<Layout title="Develop">
-				<Foot>
-					<body className="documentation"></body>
-				</Foot>
-				<iframe
-					src="https://pecanproject.github.io/pecan-documentation/develop/"
-					style={{ border: "none" }}
-					width="100%"
-					height="100%"
-					id="iFrame1"
-					// sandbox=""
-				>
-					<p>
-						<a href="/en-US/docs/Glossary" target="_blank">
-							Fallback link for browsers that don't support iframes
-						</a>
-					</p>
-				</iframe>
-			</Layout>
-		</>
-	);
+  return (
+    <>
+      <Layout title="Develop" >
+        <Foot>
+          <body className="documentation"></body>
+        </Foot>
+        <iframe
+          src="https://pecanproject.github.io/pecan-documentation/develop/"
+          style={{ border: "none" }}
+          width="100%"
+          height="100%"
+
+          id="iFrame1"
+          // sandbox=""
+        >
+          <p>
+            <a href="/en-US/docs/Glossary" target="_blank">
+              Fallback link for browsers that don't support iframes
+            </a>
+          </p>
+        </iframe>
+      </Layout>
+    </>
+  );
 }
 
 export default Develop;
